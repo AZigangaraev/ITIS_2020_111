b2caf2f59ba55aa37a8b5fd970dc634d1568b94d
Montypass <montypass@MacBook-Pro-Montypass.local>
2020-12-10 18:33:44 +0300

    control work kamaliev

---
+++ ControlWork2/ControlWork2/DetailViewController.swift
@@ -0,0 +1,89 @@
+//
+//  DetailViewController.swift
+//  ControlWork2
+//
+//  Created by Montypass on 10.12.2020.
+//
+
+import UIKit
+
+protocol DetailViewControllerDelegate: AnyObject {
+    func reloadCellData(index: Int, cell: CellData)
+    func addCellData(cell: CellData)
+}
+
+class DetailViewController: UIViewController {
+    
+    var cellData: CellData?
+    var index: Int?
+    
+    @IBOutlet weak var mainLabel: UILabel!
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var redBtn: UIButton!
+    @IBOutlet weak var blueBtn: UIButton!
+    @IBOutlet weak var yellowBtn: UIButton!
+    
+    weak var delegate: DetailViewControllerDelegate?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .save, target: self, action: #selector(saveTap))
+        
+    }
+    
+    func loadData() {
+        guard let data = cellData else {
+            return
+        }
+        mainLabel.text = data.title
+        textField.text = data.text
+    }
+    
+    @IBAction func redButtonAction(_ sender: Any) {
+        setTextForButton()
+        redBtn.setTitle("Selected Color: red", for: .normal)
+        cellData?.color = .red
+        if let index = index {
+            delegate?.reloadCellData(index: index, cell: cellData!)
+        }
+    }
+    
+    @IBAction func blueButtonAction(_ sender: Any) {
+        setTextForButton()
+        blueBtn.setTitle("Selected Color: blue", for: .normal)
+        cellData?.color = .blue
+        if let index = index {
+            delegate?.reloadCellData(index: index, cell: cellData!)
+        }
+    }
+    
+    @IBAction func yellowButtonAction(_ sender: Any) {
+        setTextForButton()
+        yellowBtn.setTitle("Selected Color: yellow", for: .normal)
+        cellData?.color = .yellow
+        if let index = index {
+            delegate?.reloadCellData(index: index, cell: cellData!)
+        }
+        
+    }
+    
+    private func setTextForButton() {
+        redBtn.setTitle("Color: red", for: .normal)
+        blueBtn.setTitle("Color: blue", for: .normal)
+        yellowBtn.setTitle("Color: yellow", for: .normal)
+    }
+    
+    @objc private func saveTap(_ sender: Any) {
+        if let index = index {
+            cellData?.text = textField.text ?? "No text"
+            delegate?.reloadCellData(index: index, cell: cellData!)
+            navigationController?.popViewController(animated: true)
+        }else {
+            cellData?.text = textField.text ?? "No text"
+            delegate?.addCellData(cell: cellData!)
+            navigationController?.popViewController(animated: true)
+        }
+    }
+    
+}
---
+++ ControlWork2/ControlWork2/ViewController.swift
@@ -0,0 +1,98 @@
+//
+//  ViewController.swift
+//  ControlWork2
+//
+//  Created by Montypass on 10.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout, DetailViewControllerDelegate {
+    
+    private var cellData = [CellData]()
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        loadData()
+    }
+    
+    private func loadData() {
+        for i in 1..<6 {
+            cellData.append(CellData(title: "№\(i)", text: "text:\(i)", color: .red))
+        }
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        cellData.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let item = collectionView.dequeueReusableCell(withReuseIdentifier: "CustomCollectionViewCell", for: indexPath) as? CustomCollectionViewCell else { fatalError("Could not deque cell") }
+        
+        item.contentView.layer.cornerRadius = 5
+        item.title.font.withSize(22)
+        item.title.textColor = .black
+        item.text.textColor = .systemGray
+        
+        item.title.text = cellData[indexPath.row].title
+        item.text.text = cellData[indexPath.row].text
+        
+        item.backgroundColor = cellData[indexPath.row].color
+        return item
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width / 2 - 12, height: 120)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        8
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        8
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let controller = detailViewController()
+        controller.cellData = cellData[indexPath.row]
+        controller.index = indexPath.row
+        controller.delegate = self
+        controller.loadViewIfNeeded()
+        controller.loadData()
+        navigationController?.pushViewController(controller, animated: true)
+    }
+    
+    private func detailViewController() -> DetailViewController {
+        guard let storyboard = storyboard else { fatalError() }
+
+        return storyboard.instantiateViewController(identifier: "DetailViewController")
+    }
+    
+    @IBAction func addNewCell(_ sender: Any) {
+        let controller = detailViewController()
+        controller.cellData = CellData(title: "Title \(cellData.count + 1)", text: "", color: .systemBlue)
+        controller.delegate = self
+        controller.loadViewIfNeeded()
+        controller.loadData()
+        navigationController?.pushViewController(controller, animated: true)
+    }
+    
+    func addCellData(cell: CellData) {
+        cellData.append(cell)
+        collectionView.reloadData()
+    }
+    
+    func reloadCellData(index: Int, cell: CellData) {
+        cellData[index] = cell
+        collectionView.reloadData()
+    }
+}
